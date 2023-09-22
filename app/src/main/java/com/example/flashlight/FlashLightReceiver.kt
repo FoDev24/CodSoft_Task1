4f310aba6cd87697fa293b91e1d1e522aefc191a
Fady Raafat <fady.raafat.ramzy@gmail.com>
2023-09-22 22:17:13 +0300

    1- setUp the layout 2- flashLight off and on functions 3- change the app icon

---
+++ app/src/main/java/com/example/flashlight/FlashLightReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.flashlight
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class FlashLightReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
